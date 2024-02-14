e3f78e1bd13ca77171030f772c39f0dfa81a820e
Vlad421 <35481094+Vlad421@users.noreply.github.com>
2024-02-14 10:47:17 -0700

    - Entry class

---
+++ app/src/main/java/my/sdlc/billmanager/Entry.java
@@ -0,0 +1,9 @@
+package my.sdlc.billmanager;
+
+import java.math.BigDecimal;
+
+abstract class Entry {
+ String category;
+
+
+}
--- app/src/main/java/my/sdlc/billmanager/helpers/dataBase/SpentTable.java
+++ app/src/main/java/my/sdlc/billmanager/helpers/dataBase/SpentTable.java
@@ -6,11 +6,14 @@ public class SpentTable extends Table{
 
     private static SpentTable st;
 
-    protected static final String TABLE_NAME = "flow";
+    protected static final String TABLE_NAME = "spent";
     protected static final String ID = "id";
     protected static final String CATEGORY = "category";
     protected static final String AMOUNT = "amount";
     protected static final String IMAGE = "image";
+    protected static final String DATE = "date";
+    protected static final String MONTH = "date_month";
+
 
     public static SpentTable getInstance() {
         if (st == null) {
